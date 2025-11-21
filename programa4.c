f7132a049d597a238d5fdcd2e428dd9e3b6d6b03
Aldanasebastian <jsaldanad@udistrital.edu.co>
2025-11-21 12:55:21 -0500

    Add files via upload

---
+++ programa4.c
@@ -0,0 +1,22 @@
+//Sebastian Aldana
+//21_10_25
+//Programa 5
+//___________________________________________________________
+
+#include <stdio.h>
+int main()
+{ 
+int entero; 
+float flotante;
+
+printf("ingresa el valor entero: ");
+scanf("%i", &entero);
+printf("el valor ingresado es: %d\n", entero);
+
+printf("Ingresa el valor float: ");
+scanf("%f", &flotante);
+printf("El valor ingresado es: %f\n", flotante);
+
+return 0; 
+
+}
